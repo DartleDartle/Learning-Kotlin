65ebfc0d3b55879f0e801818df51cd41b64c804f
DartleDartle <davide.pintore98@gmail.com>
2024-04-21 16:34:56 +0200

    Multiline strings

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,10 @@
 fun main() {
 
-    val name = "Ahmed"
-    val age = 20
-    // val msg = name + " " + age - old way
-    val msg = "Name: $name Age: $age" // new way
+    val email = """
+        Hello
+          How
+        Are You
+        """.trimIndent()
 
-    println(msg)
+    println(email)
 }
