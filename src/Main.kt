853081fef20cd8d436383c3ad776e781a88e3730
DartleDartle <davide.pintore98@gmail.com>
2024-04-21 18:07:09 +0200

    trying some mathematical classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,14 @@
+import kotlin.math.*
+
+
+
 fun main() {
 
     val number1 = 10
     val number2 = 2
-    println(number1 + number2)
-    println(number1 - number2)
-    println(number1 / number2)
-    println(number1 * number2)
-    println(number1 % number2)
+    println(PI)
+    println(max(number2,number1))
+    println(min(number2,number1))
+    println(sqrt(5.4))
+    println(round(8.2))
 }
