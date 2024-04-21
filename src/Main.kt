9597751e9af5f9f2d204b36932b162323bafcfcd
DartleDartle <davide.pintore98@gmail.com>
2024-04-21 15:14:10 +0200

    Kotlin recognize automatically the type.

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,9 @@
 fun main() {
-    var n: Int = 10
-    var l: Long = 100L
-    var d: Double = 3.14
-    var f: Float = 3.14F
-    var b: Boolean = false
-    var s: String = "String"
-    var c: Char = 'A'
+    var n: = 10
+    var l: = 100L
+    var d: = 3.14
+    var f: = 3.14F
+    var b: = false
+    var s: = "String"
+    var c: = 'A'
 }
