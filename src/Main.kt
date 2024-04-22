55869ce3a2225cf717ecaf623b97414acd8f4ce8
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-04-22 12:43:34 +0200

    And comprarison operators

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,11 @@
 import kotlin.math.*
 
 fun main() {
-
-        //For Boolean the name of the variable has to be a question.
-
-        val isAdult = true
-        val isMale = false
-        val isFemale = true
-        val ordedCompleted: Boolean? = null
-        println(ordedCompleted)
-        println(isAdult)
-        println(isMale)
-        println(isAdult && isMale)
-        println(isAdult || isMale)
-        println(isAdult && isFemale && isMale)
-        println(!isAdult)
+        val n1 = 10
+        val n2 = 20
+        println(n1 > n2)
+        println(n1 >= n2)
+        println(n1 < n2)
+        println(n1 <= n2)
+        println(n1 == n2)
 }
