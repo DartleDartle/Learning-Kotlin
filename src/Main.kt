53b0d3922427017203786e42af450d8d9202da72
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-04-22 12:52:56 +0200

    More than one condition.

--- src/Main.kt
+++ src/Main.kt
@@ -6,6 +6,8 @@ fun main() {
 
         if (n1 >= n2 || n1 <= 30) {
             println("n1 is greater than or equal to n2")
+        } else if(n1 == n2) {
+            println("n1 is equal to n2")
         } else {
             println("n1 is less than n2")
         }
