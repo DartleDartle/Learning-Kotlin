febc3982ed5cc5f96e3816bc61491a656c739153
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-04-22 12:51:50 +0200

    Again on if else statements

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,12 @@
 import kotlin.math.*
 
 fun main() {
-        val n1 = 10
+        val n1 = 31
         val n2 = 20
-        println(n1 > n2)
-        println(n1 >= n2)
-        println(n1 < n2)
-        println(n1 <= n2)
-        println(n1 == n2)
+
+        if (n1 >= n2 || n1 <= 30) {
+            println("n1 is greater than or equal to n2")
+        } else {
+            println("n1 is less than n2")
+        }
 }
