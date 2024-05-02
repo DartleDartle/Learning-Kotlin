18632ac10ec328843123658d6a625cdc0307fd65
DartleDartle <davide.pintore98@gmail.com>
2024-05-02 15:08:46 +0200

    Learning using ternary operator!

--- src/Main.kt
+++ src/Main.kt
@@ -3,17 +3,8 @@ fun main() {
     val n1 = 10
     val n2 = 20
 
-    val value = if (n1 >= n2 || n1 <= 30) {
-        // println("n1 is greater than or equal to n2")
-        "n1 is greater than or equal to n2"
-    } else if(n1 == n2) {
-        // println("n1 is equal to n2")
-        "n1 is equal to n2"
-    } else {
-        // println("n1 is less than n2")
-        "n1 is less than  n2"
-    }
-
-    println(value)
+    var r = if (n1 >= n2) ":)" else ":("
+    
+    println(r)
 
 }
