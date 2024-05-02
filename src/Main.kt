5b0b6a8ab5d2bff28a565924fcf1d4b4725dfd4b
DartleDartle <davide.pintore98@gmail.com>
2024-05-02 16:36:16 +0200

    Using 'when' statement with range of values, using example: ''in 13..19 -> ...

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,20 @@
 fun main() {
-    val gender = "M"
+    val n1 = 10
+    val n2 = 20
 
-    when(gender) {
-        "M" -> println("MALE")
-        "F" -> println("FEMALE")
-        else -> println("UNKNOWN")
+    when {
+       (n1 >= n2 || n1 <= 30) -> println(":)")
+        (n1 == 100) -> println("foo")
+        else -> println(":(")
+    }
+
+    val age = 20
+    when (age) {
+        in 13..19 -> println("Teenager")
+        !in 0..9 -> println("Not a Teenager")
+        in 20..29 -> println("Twenties")
+        in 30..39 -> println("Thirties")
+        else -> println("Over 40")
     }
 
 }
