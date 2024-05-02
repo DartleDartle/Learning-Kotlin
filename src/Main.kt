69ebbf7f021d53786f8798eef9f9bf5d4176ab8e
DartleDartle <davide.pintore98@gmail.com>
2024-05-02 17:07:01 +0200

    checking some of the different types of an empty array

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,8 @@
 fun main() {
-   val name = arrayOf("John", "Doe", "Jane", "Doe")
-    println(name[0])
-    println(name[1])
-    println(name[2])
-    println(name[3])
-    println(name.size)
 
-    if ("John" in name) {
-        println("the name is in the array")
-    } else {
-        println("the name is not in the array")
-    }
+    val arrayOfNulls = arrayOfNulls<String>(5)
+    arrayOfNulls[0] = "Hello"
+    println(arrayOfNulls.contentToString())
 
+    arrayOfNulls.fill( "*")
 }
