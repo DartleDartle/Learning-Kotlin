aa7d3e8b769afea28610fbf13b8ebd13f0570a18
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-05-05 11:56:09 +0200

    Learning about lists

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,15 @@
 fun main() {
 
-    val arrayOfNulls = arrayOfNulls<String>(5)
-    arrayOfNulls[0] = "Hello"
-    println(arrayOfNulls.contentToString())
-
-    arrayOfNulls.fill( "*")
+    // list can be expanded in every moment, compared to arrays which have fixed size, but this list isn't mutable.
+    // the syntax of list can not include the type of the elements, because it is inferred by the compiler.
+    val names: List<String> = listOf(
+        "Federico",
+        "Francesca",
+        "Alberto",
+        "Alessandro"
+    )
+    println(names)
+    println(names[0])
+    println(names.size)
+    // list are built on arrays, so they have the same performance.
 }
