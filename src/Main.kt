bc8a322d4ac0c722da4edc0473b7f440408d0df6
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-05-05 14:08:09 +0200

    Creating and invoking functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,10 @@
+
+// creating and invoking function
+
 fun main() {
-
-    val nums = intArrayOf(1, 2, 3, 4, 5, 6, 8)
-
-    // break and continue
-    for (n in nums) {
-        if (n % 2 != 0) continue
-        if (n > 5) break
-        println(n)
-    }
-    // break and continue only in a loop
-    nums.forEach {
-        if (it % 2 != 0) return@forEach
-        if (it > 5) return@forEach
-        println(it)
-    }
+    greet()
+}
+// by default functions are public and final, and even unit return type is optional
+fun greet(){
+    println("Hello, World!")
 }
