a49e1d73a30fe998ae081372f55448fa00cdfe3e
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-05-05 12:10:29 +0200

    Learning destruturing with Triple, custom, Pair, and variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,54 @@
 fun main() {
 
-    // Now this is a mutable list.
-    val names = mutableListOf(
+    // Destructuring
+    val names = listOf(
         "Federico",
         "Francesca",
         "Alberto",
         "Alessandro"
     )
-    println(names)
-    println(names[0])
-    println(names.size)
-    //removing an element
-    names.remove("Federico")
-    println(names)
-    //adding an element
-    names.add("Federico")
-    println(names)
+    val (a, b, c, d) = names
+
+    /*
+    val j = names[0]
+    val k = names[1]
+    val l = names[2]
+    val m = names[3]
+    */
+
+println("$a $b $c $d")
+
+    // Destructuring with Pair
+    val pair = Pair("Federico", "Zanetello")
+    val (name, surname) = pair
+
+    /*
+    val name = pair.first
+    val surname = pair.second
+    */
+    println("$name $surname")
+
+    // Destructuring with Triple
+    val triple = Triple("Federico", "Zanetello", 30)
+    val (name2, surname2, age) = triple
+
+    /*
+    val name2 = triple.first
+    val surname2 = triple.second
+    val age = triple.third
+    */
+
+    println("$name2 $surname2 $age")
+
+    // Destructuring with custom class
+    val person = Person("Federico", "Zanetti", 30)
+    val (name3, surname3, age2) = person
+
+    /*
+    val name3 = person.name
+    val surname3 = person.surname
+    val age2 = person.age
+    */
+    println("$name3 $surname3 $age2")
+
 }
