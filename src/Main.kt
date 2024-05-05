d9449958d3e48d36ff7f028c11458555a96dc69e
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-05-05 13:28:24 +0200

    .forEach construct

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,7 @@
 fun main() {
 
-    // Range, Down to, Steps
-
-    println("Range")
-    for (i in 1..5) println(i)
-    println("Down to")
-    for (i in 5 downTo 1) println(i)
-    println("Steps")
-    for (i in 1..5 step 2) println(i)
-    println("Steps and down to")
-    for (i in 5 downTo 1 step 2) println(i)
+    // .forEach
+    val names = arrayOf("John", "Jim", "Jane", "Jack")
+    names.forEach { println(it) }
+    for (name in names) println(name)
 }
