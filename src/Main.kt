af3c150f5c26fcd139a0d237111a3631fd30a5af
DartleDartle <davide.pintore98@gmail.com>
2024-06-04 15:12:16 +0200

    Classes and Objects overview

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,13 @@
 fun main() {
+    // in Java you need to use "New" keyword to create an object, here you don't need to use "New" keyword.
+    // Objects
+    val tv = SmartDevice()
+    val phone = SmartDevice()
 
-    println(twoValues())
-    println(threeValues())
-
-    // you can even destructure the values
-
-    val (v1, v2) = twoValues()
-    println("$v1, $v2")
-
-    val (t1, t2, t3) = threeValues()
-    println("$t1, $t2, $t3")
 }
 
-fun twoValues(): Pair<String, Int> =
-    "Mario" to 30
-
-
-fun threeValues(): Triple<String, Int, Char> =
-     Triple(
-            "Mario",
-            30,
-            'M'
-     )
+// Blueprint
+class SmartDevice {
+    // Properties. variables. val or var.
+    // Behavior. functions. fun.
+}
