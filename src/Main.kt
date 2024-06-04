5835818ae8449820fc6ff25ef9ce53919e8585df
DartleDartle <davide.pintore98@gmail.com>
2024-06-04 14:59:59 +0200

    Return multiple values from functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,34 +1,24 @@
 fun main() {
-    greet("John" )
-    greet("Carol", 15)
-    greet("Alex", 27)
 
-    print(double(5))
+    println(twoValues())
+    println(threeValues())
+
+    // you can even destructure the values
+
+    val (v1, v2) = twoValues()
+    println("$v1, $v2")
+
+    val (t1, t2, t3) = threeValues()
+    println("$t1, $t2, $t3")
 }
 
-// You cannot declare the type only if is a single expression
-
-fun double(n: Int) = n * 2
-
-/* fun double(x: Int) : Int {
-    return x * 2
-} */
-
-fun greet(name: String, age: Int = -1){
-
-    println("Hello $name")
-
-    if (age == -1){
-
-        println("$name please provide your age\n")
-        return
-    }
-
-    if (age >= 18){
-        println("$name $age - You are an adult\n")
-    } else {
-        println("$name You are a minor\n")
-    }
-}
+fun twoValues(): Pair<String, Int> =
+    "Mario" to 30
 
 
+fun threeValues(): Triple<String, Int, Char> =
+     Triple(
+            "Mario",
+            30,
+            'M'
+     )
