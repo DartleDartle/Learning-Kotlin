7dd46c87c266010c3a43eb6fd0db389c56faae49
DartleDartle <davide.pintore98@gmail.com>
2024-06-04 15:23:17 +0200

    Get and Set Behavior

--- src/Main.kt
+++ src/Main.kt
@@ -4,20 +4,28 @@ fun main() {
     val tv = SmartDevice()
 
     tv.brand = "Samsung Smart TV 55' QLED 4K"
-    tv.price = 1000.0
+    tv.price = 999.99
+    println("${tv.brand} costs ${tv.price}$")
 
     val phone = SmartDevice()
 
     phone.brand = "Apple Iphone 12 Pro Max"
-    phone.price = 1200.0
-    
+    phone.price = 1199.99
+    println("${phone.brand} costs ${phone.price}$")
+
 
 }
 
 // Blueprint
 class SmartDevice {
     // Properties. variables. val or var.
+
     var brand: String = ""
+        // get() = "foo"
+        set(value) {
+            field = value.uppercase()
+        }
     var price: Double = 0.0
+
     // Behavior. functions. fun.
 }
