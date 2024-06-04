449b66ada2563b9e204892363a21c603a7102bbb
DartleDartle <davide.pintore98@gmail.com>
2024-06-04 15:53:56 +0200

    Creating constructors and populating them

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,19 @@
 fun main() {
     // in Java you need to use "New" keyword to create an object, here you don't need to use "New" keyword.
     // Objects
-    val tv = SmartDevice()
-
-    tv.brand = "Samsung Smart TV 55' QLED 4K"
-    tv.price = 999.99
+    val tv = SmartDevice(
+        "Samsung Smart TV 55' QLED 4K",
+        999.99
+    )
     println("${tv.brand} costs ${tv.price}$")
     tv.turnOn()
     tv.getDeviceState()
 
 
-    val phone = SmartDevice()
-
-    phone.brand = "Apple Iphone 12 Pro Max"
-    phone.price = 1199.99
+    val phone = SmartDevice(
+        "Samsung Galaxy S21 Ultra",
+        1199.99
+    )
     println("${phone.brand} costs ${phone.price}$")
     phone.turnOff()
     phone.getDeviceState()
@@ -21,13 +21,11 @@ fun main() {
 }
 
 // Blueprint
-class SmartDevice {
-    // Properties. variables. val or var.
-
-    var brand: String = ""
-    var price: Double = 0.0
+class SmartDevice(
+    var brand: String,
+    var price: Double,
     var isSwitchedOn: Boolean = false
-
+) {
     // Behavior. functions. fun.
 
     fun getDeviceState(): String {
